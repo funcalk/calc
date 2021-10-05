91a8ed3f3ec90a2b4d4315820bfe503d0ab4fefd
Oleg Alexeyev <oleg.alexeyev@f-secure.com>
2021-10-05 10:59:52 +0300

    Java and Kotlin are on the same target version, gradle doesn't complain anymore Sources jar is included

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,43 @@
+import org.gradle.api.JavaVersion.VERSION_1_8
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
+
 plugins {
-    application
-    kotlin("jvm") version "1.5.31"
+  application
+  kotlin("jvm") version "1.5.31"
 }
 
 group = "org.funcalk"
 version = "1.0"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation(kotlin("stdlib-jdk8"))
+  implementation(kotlin("stdlib-jdk8"))
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
-    testImplementation("org.assertj:assertj-core:3.21.0")
-    testImplementation(kotlin("test"))
+  testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+  testImplementation("org.assertj:assertj-core:3.21.0")
+  testImplementation(kotlin("test"))
 }
 
 tasks.named<Test>("test") {
-    useJUnitPlatform()
+  useJUnitPlatform()
+}
+
+java {
+  withSourcesJar()
+  sourceCompatibility = VERSION_1_8
+  targetCompatibility = VERSION_1_8
+}
+
+val compileKotlin: KotlinJvmCompile by tasks
+compileKotlin.kotlinOptions {
+  javaParameters = true
+  jvmTarget = VERSION_1_8.toString()
 }
 
 application {
-    mainClass.set("org.calc.REPLKt")
+  mainClass.set("org.calc.REPLKt")
 }
