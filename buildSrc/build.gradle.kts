73f5fc61c1dedc1b6f5eb9b10813fc7f821391b5
Oleg Alexeyev <oleg.alexeyev@f-secure.com>
2022-01-04 01:20:47 +0300

    Set Kotlin and Java target level to the same value to avoid warning

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.api.JavaVersion.VERSION_1_8
+
 plugins {
     `kotlin-dsl`
 }
@@ -9,4 +11,20 @@ repositories {
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     implementation("gradle.plugin.com.zoltu.gradle.plugin:git-versioning:3.0.3")
+}
+
+tasks {
+    java {
+        sourceCompatibility = VERSION_1_8
+        targetCompatibility = VERSION_1_8
+    }
+    kotlin {
+        listOf(compileKotlin, compileTestKotlin).forEach { task ->
+            task.configure {
+                kotlinOptions {
+                    jvmTarget = VERSION_1_8.toString()
+                }
+            }
+        }
+    }
 }
